3e549e215e888f81ee558a0901133213b829b31e
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2025-09-16 13:22:15 -0300

    algunas practicas hechas

---
+++ Programacion_3/JavaScripct/prueba/JavaScript_I.js
@@ -0,0 +1,56 @@
+/*console.log("Hola mundo desde JavaScript");*/
+
+// alert("obi wan kenobi");
+
+// alert(mensaje = saluda());
+
+// FUNCION DEFINIDA
+/* 
+saluda();
+
+function saluda() {
+    console.log("Hola mundo!");
+    let mensaje2 = "Hola mundo!";
+    
+    return mensaje2;
+    }
+    */
+
+// FUNCION EXPRESADA
+// const saludame = function(){
+//     console.log("sexp");
+// }
+// saludame()
+    
+// FUNCION ANONIMA
+// setTimeout(function() {
+//     console.log("Soy una funcion anonima y asincronica!");
+// }, 1000);
+
+// FUNCION DE METODO
+// const persona = {
+//     nombre : "Miguel",
+
+//     saludar(){
+//         console.log(`Hola! me llamo ${this.nombre}`);
+        
+//     },
+//     golpear(){
+//         console.log("Te estoy golpeando");
+        
+//     }
+// }
+// persona.saludar();
+// persona.golpear();
+
+// FUNCION INMEDIATA
+
+//  (function() {
+//         console.log("Soy una funcion IIFE");
+//     }())
+
+// // FUNCION FLECHA
+//   const sumarFlecha = (a, b) => a + b;
+//     console.log(sumarFlecha(2, 3));
+
+  
