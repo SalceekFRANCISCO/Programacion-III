5a8b54dc133e27447cd32f4f011a63e10e3309e8
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2025-11-04 17:01:39 -0300

    sos

---
+++ tpIntegradorBack/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+import enviroments from "./src/api/config/environment.js";
+
+const PORT = enviroments.port;
+
+app.get("/",(req,res) => {
+    res.send("hola mundo desde express.js")
+});
+
+app.listen(PORT, ()=>{
+    console.log(`servidor corriendo desde el puerto ${PORT}`)
+});
+
+
