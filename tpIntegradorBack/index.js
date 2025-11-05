5df3613adfec857b07fd42a7b88fcc9d0d75352c
franciscosalceek1 <salceekmunozfrancisco@gmail.com>
2025-11-05 11:19:49 -0300

    actualizaciones

--- tpIntegradorBack/index.js
+++ tpIntegradorBack/index.js
@@ -2,14 +2,46 @@ import express from "express";
 const app = express();
 
 import enviroments from "./src/api/config/environments.js";
+import connection from "./src/api/database/db.js";
 const PORT = enviroments.port;
 
 app.get("/",(req,res) => {
     res.send("hola mundo desde express.js")
 });
 
-app.listen(PORT, ()=>{
-    console.log(`servidor corriendo desde el puerto ${PORT}`)
+
+/*==================================================
+ENDPOINTS
+==================================================*/
+app.get("/",(req, res)=>{
+    res.send("TP integrador Div 132");
+})
+
+
+app.get("./productos", async(req, res)=>{
+    try{
+        const sql = "SELECT * FROM PRODUCTOS"
+        
+        // la conexion devuelve dos campos
+        const [rows, fields] = await connection.query(sql);
+        
+        res.status(200).json({
+            payload: rows
+        });
+        
+    }
+    catch(error){
+        console.log("Error obteniendo productos"+error.message);
+        
+        res.status(500).json({
+            message: "error al intentar obtener productos"
+        })
+        
+    }
+
 });
 
 
+app.listen(PORT, ()=>{
+    console.log(`servidor corriendo desde el puerto ${PORT}`)
+});
---
+++ tpIntegradorBack/src/api/database/db.js
@@ -0,0 +1,15 @@
+import mysql2 from "mysql2/promise";
+
+import enviroments from "../config/environments.js"
+
+const {database} = enviroments;
+
+
+const connection = mysql.createPool({
+    host: database.host,
+    database: database.name,
+    user: database.user,
+    password: database.password,
+});
+
+export default connection;
